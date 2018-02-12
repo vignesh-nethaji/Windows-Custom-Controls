f79226abcd8287b12875ab4862211ca7bc8dd69b
Vignesh Nethaji <vigneshnethajichengam@gmail.com>
2018-02-12 14:08:22 +0530

    Adding the New Context Menu Strip With Menporul UI

---
+++ Menporul.Windows/Menporul.Windows.Controls/Controls/MenContextMenuStrip.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Menporul.Windows.Controls.Core;
+namespace Menporul.Windows.Controls
+{
+    /// <summary>
+    /// Menporul MenContextMenuStrip
+    /// </summary>
+    /// <author>Vignesh Nethaji</author>
+    public class MenContextMenuStrip : ContextMenuStrip
+    {
+        public MenContextMenuStrip()
+        {
+            this.RenderMode = ToolStripRenderMode.Professional;
+            this.Renderer = new MenMenuStripColorRenderer();
+        }
+    }
+}
