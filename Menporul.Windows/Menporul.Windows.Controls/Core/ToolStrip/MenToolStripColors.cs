9f2a8c46b90ad70058ea998010a018d0e6672866
Vignesh Nethaji <vigneshnethajichengam@gmail.com>
2018-02-12 14:20:27 +0530

    Override the professional Color Table for modify the Theme Appearance of ToolStrip .

---
+++ Menporul.Windows/Menporul.Windows.Controls/Core/ToolStrip/MenToolStripColors.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menporul.Windows.Controls.Core
+{
+    /// <summary>
+    /// Menporul ProfessionalColorTable
+    /// </summary>
+    /// <author>Vignesh Nethaji</author>
+    internal class MenToolStripColors : ProfessionalColorTable
+    {
+        public override Color MenuItemSelectedGradientBegin
+        {
+
+            get { return MenConstants.ActiveBackGround; }
+        }
+
+        public override Color MenuItemSelectedGradientEnd
+        {
+            get { return MenConstants.ActiveBackGround; }
+        }
+
+        public override Color ToolStripDropDownBackground
+        {
+            get
+            {
+                return MenConstants.Light;
+            }
+        }
+        public override Color ImageMarginGradientBegin
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }
+        public override Color ImageMarginGradientMiddle
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }
+        public override Color ImageMarginGradientEnd
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }
+        public override Color ToolStripBorder
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }        
+        public override Color ToolStripGradientEnd
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }
+        public override Color ToolStripGradientMiddle
+        {
+            get
+            {
+                return MenConstants.Thin;
+            }
+        }
+        public override Color ButtonSelectedBorder
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonSelectedGradientBegin
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonSelectedGradientEnd
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonSelectedGradientMiddle
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonSelectedHighlight
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonSelectedHighlightBorder
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonPressedBorder
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+
+        public override Color ButtonPressedGradientBegin
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonPressedGradientEnd
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonPressedGradientMiddle
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonPressedHighlight
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;
+            }
+        }
+        public override Color ButtonPressedHighlightBorder
+        {
+            get
+            {
+                return MenConstants.ActiveBackGround;                
+            }
+        }
+       
+    }
+}
